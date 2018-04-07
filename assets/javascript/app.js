616bce91364f5c895981647c3f1d27485e3766b6
Trey <treyw1850@gmail.com>
2018-04-07 00:09:11 -0700

    basic layout

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+
+
+
+// my giphy api key Y4xlv8FyBnZbqm0KrrYBv04TFs0MkfZ2
+
+
+  $(document).ready(function() {
+    $('#carouselExampleIndicators').carousel({
+      interval: 1200
+    });
+  });
+  
+  let topics = [];
+
+  
